b9fb96a84fcfd343909bdc94615aa15d83a9676b
oohdimas <dimasfaid@gmail.com>
2021-09-19 19:16:01 +0700

    Flow tipis tipis

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        /* Kamus */
+        int command;
+        Scanner scanner = new Scanner(System.in);
+        
+        /* Algoritma */
+        do {
+            System.out.println("MENU");
+            System.out.println("1. Sistem Persamaan Linier");
+            System.out.println("2. Determinan");
+            System.out.println("3. Matriks balikan");
+            System.out.println("4. Interpolasi Polinom");
+            System.out.println("5. Regresi linier berganda");
+            System.out.println("6. Keluar");
+
+            command = scanner.nextInt();
+            switch (command) {
+                case 1:
+                    SPL();
+                    break;
+            
+                case 2:
+                    Determinan();
+                    break;
+                
+                case 3:
+                    Balikan();
+                    break;
+            
+                case 4:
+                    
+                    break;
+                
+                case 5:
+                    
+                    break;
+            }
+        } while (command != 6);
+    }
+
+    public static void SPL() {
+        System.out.println("1. Metode eliminasi Gauss");
+        System.out.println("2. Metode eliminasi Gauss-Jordan");
+        System.out.println("3. Metode matriks balikan");
+        System.out.println("4. Metode Cramer");
+        
+    }
+
+    public static void Determinan() {
+        System.out.println("1. Metode reduksi baris");
+        System.out.println("2. Metode ekspansi kofaktor");
+        
+        
+    }
+
+    public static void Balikan() {
+        System.out.println("1. Metode adjoin");
+        System.out.println("2. Metode matriks identitas");
+           
+    }
+}
