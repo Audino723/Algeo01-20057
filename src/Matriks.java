d1c975aa9780649381cf9b13dfe2cc4a5ccb3997
oohdimas <dimasfaid@gmail.com>
2021-09-22 22:52:06 +0700

    coba bikin det reduksi baris

--- src/Matriks.java
+++ src/Matriks.java
@@ -21,4 +21,38 @@ public class Matriks {
             }
         }
     }
+
+    public double detReduksiBaris() {
+        int i,j,k;
+        double ratio, det;
+        Matriks mTemp;
+        int n = this.row;
+
+        mTemp = this;
+
+        if (mTemp.col != mTemp.row || mTemp.row != mTemp.col + 1){
+            System.out.println("Ukuran matriks tidak didukung untuk dicari determinannya");
+            det = 0.0;
+        }else{
+            for (i = 0; i < n-1; i++) {
+                if (mTemp.Mat[i][i]==0.0) {
+                    det = 0.0;
+                    break;
+                } else {
+                    for (j = i + 1; j < n; j++) {
+                        ratio = mTemp.Mat[j][i] / mTemp.Mat[i][i];
+
+                        for (k = 0; k <n ; k++) {
+                            mTemp.Mat[j][k] = mTemp.Mat[j][k] - ratio * mTemp.Mat[i][k];
+                        }
+                    }
+                }
+            
+            }
+            det
+        }
+        
+        
+        return det;
+    }
 }
